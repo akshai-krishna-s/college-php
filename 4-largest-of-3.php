204546ed0a8544e500a2a255813705a883a90d67
Akshai Krishna S <akshai404akz@gmail.com>
2024-01-07 21:02:52 +0530

    Fix comparison bug in largest-of-3.php

--- 4-largest-of-3.php
+++ 4-largest-of-3.php
@@ -22,7 +22,7 @@
       $num2 = $_POST['num2'];
       $num3 = $_POST['num3'];
 
-      if ( $num1 >= $num2 && $num3 >= $num3 ) echo "$num1 is the largest";
+      if ( $num1 >= $num2 && $num1 >= $num3 ) echo "$num1 is the largest";
       else if ( $num2 >= $num1 && $num2 >= $num3 ) echo "$num2 is the largest";
       else echo "$num3 is the largest";
     }
